0a07f0a4214bb005e191d1a2bebe46532895ec05
Keyology <keonimurray45@gmail.com>
2019-03-15 16:36:58 -0700

    added GET route and now serving statice content

--- main.go
+++ main.go
@@ -8,5 +8,14 @@ import (
 func main() {
 	PORT := "127.0.0.1:8080"
 	log.Println(`listening on port` + PORT)
-	log.Fatal(http.ListenAndServe(PORT, nil), `listeing on port:`, PORT)
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	log.Fatal(http.ListenAndServe(PORT, nil))
 }
+
+//CompleteTaskFunc will get the taks id and send it to the client
+// func CompleteTaskFunc(w http.ResponseWriter, r *http.Request) {
+// 	if r.Method == "GET" {
+// 		id := r.URL.Path[len("/tasks"):]
+// 		w.Write([]byte("Get the task " + id))
+// 	}
+// }
