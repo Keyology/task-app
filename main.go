1ce23bca93282752ddab397e07b44c21ebe1a54f
Keyology <keonimurray45@gmail.com>
2019-03-15 16:37:29 -0700

    added comments

--- main.go
+++ main.go
@@ -6,9 +6,13 @@ import (
 )
 
 func main() {
+	// create a port for the server to connect
 	PORT := "127.0.0.1:8080"
+	// prints the server is listening on port 8080
 	log.Println(`listening on port` + PORT)
+	// net/http method that handles servering statice content
 	http.Handle("/", http.FileServer(http.Dir("public")))
+	// connects to  port 8080
 	log.Fatal(http.ListenAndServe(PORT, nil))
 }
 
